1f49201c4750b8c5a499b97e80374645c8f712a2
Stephen Hughes <shughes90@live.com>
2023-11-08 08:52:30 -0700

    Completed Variables and Strings

---
+++ Variables and Strings/mileage_converter.py
@@ -0,0 +1,6 @@
+# This is to practice with variables and data types
+
+kilos_run = input("How many kilomileters did you run today? ")
+miles = float(kilos_run) / 1.60934
+miles = round(miles, 2)
+print(f"{kilos_run} kilometers is equal to {miles} miles!")
