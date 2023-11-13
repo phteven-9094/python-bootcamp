5649d11061f602b92b2180d7d502d76cc185bbc1
Stephen Hughes <shughes90@live.com>
2023-11-13 08:51:46 -0700

    Complete 'Looping in Python'

---
+++ Looping in Python/while_loop_exercises.py
@@ -0,0 +1,38 @@
+# Simple example
+# msg = input("What's the secret password?")
+# while msg != "bananas":
+#     print("WRONG!")
+#     msg = input("What's the secret password?")
+# print("CORRECT!")
+
+# Convert For Loop to While Loop
+# for num in range(1, 11):
+#     print(num)
+
+# num = 1
+# while num < 11:
+#     print(num)
+#     num += 1
+
+# EXERCISE: Emoji Art
+# For Loop Version
+# for num in range(1, 11):
+#     print("\U0001f600" * num)
+# # While Loop Version
+# times = 1
+# while times <= 10:
+#     print("\U0001f600" * times)
+#     times += 1
+
+#EXERCISE: Stop Copying Me
+# response = input("Hey, how's it going? ")
+# while response != "stop copying me":
+#     print(response)
+#     response = input()
+# print("UGH FINE YOU WIN")
+
+#Controlled Loop Exit
+while True:
+    command = input("Type 'exit' to exit")
+    if command == "exit":
+        break
