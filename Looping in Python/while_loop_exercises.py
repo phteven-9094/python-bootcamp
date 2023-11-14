5302cfe1f556a4ff973fb66812f5a54436c4670e
Stephen Hughes <shughes90@live.com>
2023-11-14 08:01:08 -0700

    Completed Guessing Game

--- Looping in Python/while_loop_exercises.py
+++ Looping in Python/while_loop_exercises.py
@@ -1,35 +1,35 @@
 # Simple example
-# msg = input("What's the secret password?")
-# while msg != "bananas":
-#     print("WRONG!")
-#     msg = input("What's the secret password?")
-# print("CORRECT!")
+msg = input("What's the secret password?")
+while msg != "bananas":
+    print("WRONG!")
+    msg = input("What's the secret password?")
+print("CORRECT!")
 
 # Convert For Loop to While Loop
-# for num in range(1, 11):
-#     print(num)
+for num in range(1, 11):
+    print(num)
 
-# num = 1
-# while num < 11:
-#     print(num)
-#     num += 1
+num = 1
+while num < 11:
+    print(num)
+    num += 1
 
 # EXERCISE: Emoji Art
 # For Loop Version
-# for num in range(1, 11):
-#     print("\U0001f600" * num)
-# # While Loop Version
-# times = 1
-# while times <= 10:
-#     print("\U0001f600" * times)
-#     times += 1
+for num in range(1, 11):
+    print("\U0001f600" * num)
+# While Loop Version
+times = 1
+while times <= 10:
+    print("\U0001f600" * times)
+    times += 1
 
 #EXERCISE: Stop Copying Me
-# response = input("Hey, how's it going? ")
-# while response != "stop copying me":
-#     print(response)
-#     response = input()
-# print("UGH FINE YOU WIN")
+response = input("Hey, how's it going? ")
+while response != "stop copying me":
+    print(response)
+    response = input()
+print("UGH FINE YOU WIN")
 
 #Controlled Loop Exit
 while True:
