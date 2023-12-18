1db4d3d244f8e73f0836cb0af03094b255903596
Stephen Hughes <shughes90@live.com>
2023-12-18 15:34:24 -0700

    Began OOP Section

---
+++ OOP Section/OOP.py
@@ -0,0 +1,18 @@
+#Defining Classes and Objects
+
+#Classes
+    #Like blueprints for objects. Classes can contain methods (functions) and attributes (Similar to keys in a dict)
+#Instace
+    # Objects that are constructed from a class blueprint that contains their class's methods and properties
+#Encapsulation
+    #the grouping of public and private attributes and methods into a programmatic class, making abstraction possible
+#Abstraction
+    #exposing only "relevant" data in a class interface, hiding private attributes and methods from users
+
+#Creating Classes and Instances
+    #see class_examples.py
+
+#The __init__method
+    #Anytime you make a new instance of a class, Python will automatically look for an __init__ method
+    #which gets called every time you create an instance of the class (instantiate)
+    #You don't need to use an __init__ method if you are creating a class with only methods
---
+++ OOP Section/class_examples.py
@@ -0,0 +1,8 @@
+
+
+class User:
+    def __init__(self, first, last, age):
+        #If you instatiate a class, you would have to make sure to pass in the above, similar to a function
+        self.first = first
+        self.last = last
+        self.age = age
