0660b86410e78a2d0df1744e14163130dcf25e2a
Stephen Hughes <shughes90@live.com>
2024-01-02 11:38:47 -0700

    Complete OOP Part 2

--- OOP Section Part 2/OOP2.py
+++ OOP Section Part 2/OOP2.py
@@ -1,9 +1,27 @@
-#Inheritance
+# Inheritance
     # A key feature of OOP is the ability to define a class which inherits from another class (A "base" or "parent" class)
     # In Python, inheritance works by passing the parent calss as an argument to the definition of a child class
 
-#Properties
+# Properties
     #See human_class.py
 
-#Intro to Super()
+# Intro to Super()
     # If you use super(), it will pull from the parent class of the current class
+    
+# Multiple Inheritance
+    # You can pass a 2nd class when creating a new class to inherit from both of the parent classes
+    # Don't have to use it; fairly rare and tends to complicate things
+    
+# Polymorphism
+    #A key principle in OOP is the idea of polymorphism - an object can take on many forms
+    #Examples:
+        # 1: The same class method works in a similar way for different classes
+            # Cat.speak()
+            # Dog.speak()
+            # Human.speak()
+        #2: The same operation works for different kinds of objects such as len()
+    # 1 type The same class method works in a similar way for different classes
+    # 2nd type: The same operation works for different kinds of objects such as the + sign (Adding and Concatenation)
+        #Called special methods
+        #See special methods file
+    
